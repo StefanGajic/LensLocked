b696090491517440dbb051718c969bb063817842
sonja <stefan.gajic@ymail.com>
2019-12-13 10:13:10 +0100

    Users can upload and delete images

--- models/galleries.go
+++ models/galleries.go
@@ -2,34 +2,40 @@ package models
 
 import "github.com/jinzhu/gorm"
 
-// const (
-// 	ErrUserIDRequired modelError = "models: user ID is required"
-// 	ErrTitleRequired  modelError = "models: title is required"
-// )
-
 // Gallery represents the galleries table in our DB
 // and is mostly a container resource composed of images.
 type Gallery struct {
 	gorm.Model
-	UserID uint   `gorm:"not_null;index"`
-	Title  string `gorm:"not_null"`
+	UserID uint    `gorm:"not_null;index"`
+	Title  string  `gorm:"not_null"`
+	Images []Image `gorm:"-"`
 }
 
-func NewGalleryService(db *gorm.DB) GalleryService {
-	return &galleryService{
-		GalleryDB: &galleryValidator{
-			GalleryDB: &galleryGorm{
-				db: db,
-			},
-		},
+func (g *Gallery) ImagesSplitN(n int) [][]Image {
+	// Create out 2D slice
+	ret := make([][]Image, n)
+	// Create the inner slices - we need N of them, and we will
+	// start them with a size of 0.
+	for i := 0; i < n; i++ {
+		ret[i] = make([]Image, 0)
 	}
+	// Iterate over our images, using the index % n to determine
+	// which of the slices in ret to add the image to.
+	for i, img := range g.Images {
+		// % is the remainder operator in Go, eg:
+		//    0%3 = 0
+		//    1%3 = 1
+		//    2%3 = 2
+		//    3%3 = 0
+		//    4%3 = 1
+		//    5%3 = 2
+		bucket := i % n
+		ret[bucket] = append(ret[bucket], img)
+	}
+	return ret
 }
 
 type GalleryService interface {
-	GalleryDB
-}
-
-type galleryService struct {
 	GalleryDB
 }
 
@@ -47,6 +53,20 @@ type GalleryDB interface {
 	Create(gallery *Gallery) error
 	Update(gallery *Gallery) error
 	Delete(id uint) error
+}
+
+func NewGalleryService(db *gorm.DB) GalleryService {
+	return &galleryService{
+		GalleryDB: &galleryValidator{
+			GalleryDB: &galleryGorm{
+				db: db,
+			},
+		},
+	}
+}
+
+type galleryService struct {
+	GalleryDB
 }
 
 type galleryValidator struct {
