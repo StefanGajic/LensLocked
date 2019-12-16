26d79e7f5c41621fae6b006659f44f1d1d8091da
sonja <stefan.gajic@ymail.com>
2019-12-16 16:55:28 +0100

    Cleaned errors, added CSRF protection, made config vars, made users log out and redirected with alerts

--- main.go
+++ main.go
@@ -1,28 +1,35 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net/http"
 
 	"github.com/lenslocked/middleware"
+	"github.com/lenslocked/rand"
 
+	"github.com/gorilla/csrf"
 	"github.com/gorilla/mux"
 	"github.com/lenslocked/controllers"
 	"github.com/lenslocked/models"
 )
 
-const (
-	host     = "localhost"
-	port     = 5432
-	user     = "postgres"
-	password = "postgres"
-	dbname   = "lenslocked_dev"
-)
-
 func main() {
-	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
-		host, port, user, password, dbname)
-	services, err := models.NewServices(psqlInfo)
+
+	boolPtr := flag.Bool("prod", false, "Provide this flag "+
+		"in production. This ensures that a .config file is "+
+		"provided before the application starts.")
+	flag.Parse()
+	cfg := LoadConfig(*boolPtr)
+	dbCfg := cfg.Database
+
+	services, err := models.NewServices(
+		models.WithGorm(dbCfg.Dialect(), dbCfg.ConnectionInfo()),
+		models.WithLogMode(!cfg.IsProd()),
+		models.WithUser(cfg.Pepper, cfg.HMACKey),
+		models.WithGallery(),
+		models.WithImage(),
+	)
 	if err != nil {
 		panic(err)
 	}
@@ -35,6 +42,13 @@ func main() {
 	usersC := controllers.NewUsers(services.User)
 	galleriesC := controllers.NewGalleries(services.Gallery, services.Image, r)
 
+	b, err := rand.Bytes(32)
+	if err != nil {
+		panic(err)
+	}
+
+	csrfMw := csrf.Protect(b, csrf.Secure(cfg.IsProd()))
+
 	userMw := middleware.User{
 		UserService: services.User,
 	}
@@ -46,43 +60,36 @@ func main() {
 	r.HandleFunc("/signup", usersC.Create).Methods("POST")
 	r.Handle("/login", usersC.LoginView).Methods("GET")
 	r.HandleFunc("/login", usersC.Login).Methods("POST")
+	r.Handle("/logout", requireUserMw.ApplyFn(usersC.Logout)).Methods("POST")
 
 	// Image routes
 	imageHandler := http.FileServer(http.Dir("./images/"))
 	r.PathPrefix("/images/").Handler(http.StripPrefix("/images/", imageHandler))
 
 	// Gallery routes
-	r.Handle("/galleries",
-		requireUserMw.ApplyFn(galleriesC.Index)).
-		Methods("GET").
+	r.Handle("/galleries", requireUserMw.ApplyFn(galleriesC.Index)).Methods("GET").
 		Name(controllers.IndexGalleries)
-	r.Handle("/galleries/new",
-		requireUserMw.Apply(galleriesC.New)).
-		Methods("GET")
-	r.Handle("/galleries",
-		requireUserMw.ApplyFn(galleriesC.Create)).
-		Methods("POST")
-	r.HandleFunc("/galleries/{id:[0-9]+}",
-		galleriesC.Show).
-		Methods("GET").
+	r.Handle("/galleries/new", requireUserMw.Apply(galleriesC.New)).Methods("GET")
+	r.Handle("/galleries", requireUserMw.ApplyFn(galleriesC.Create)).Methods("POST")
+	r.HandleFunc("/galleries/{id:[0-9]+}", galleriesC.Show).Methods("GET").
 		Name(controllers.ShowGallery)
-	r.HandleFunc("/galleries/{id:[0-9]+}/edit",
-		requireUserMw.ApplyFn(galleriesC.Edit)).
-		Methods("GET").
+	r.HandleFunc("/galleries/{id:[0-9]+}/edit", requireUserMw.ApplyFn(galleriesC.Edit)).Methods("GET").
 		Name(controllers.EditGallery)
-	r.HandleFunc("/galleries/{id:[0-9]+}/update",
-		requireUserMw.ApplyFn(galleriesC.Update)).
+	r.HandleFunc("/galleries/{id:[0-9]+}/update", requireUserMw.ApplyFn(galleriesC.Update)).
 		Methods("POST")
-	r.HandleFunc("/galleries/{id:[0-9]+}/delete",
-		requireUserMw.ApplyFn(galleriesC.Delete)).
+	r.HandleFunc("/galleries/{id:[0-9]+}/delete", requireUserMw.ApplyFn(galleriesC.Delete)).
 		Methods("POST")
-	r.HandleFunc("/galleries/{id:[0-9]+}/images",
-		requireUserMw.ApplyFn(galleriesC.ImageUpload)).
+	r.HandleFunc("/galleries/{id:[0-9]+}/images", requireUserMw.ApplyFn(galleriesC.ImageUpload)).
 		Methods("POST")
-	r.HandleFunc("/galleries/{id:[0-9]+}/images/{filename}/delete",
-		requireUserMw.ApplyFn(galleriesC.ImageDelete)).
+	r.HandleFunc("/galleries/{id:[0-9]+}/images/{filename}/delete", requireUserMw.ApplyFn(galleriesC.ImageDelete)).
 		Methods("POST")
 
-	fmt.Println("Starting the server on :3000...")
-	http.ListenAndServe(":3000", userMw.Apply(r))
+	// Assets
+	assetHandler := http.FileServer(http.Dir("./assets/"))
+	assetHandler = http.StripPrefix("/assets/", assetHandler)
+	r.PathPrefix("/assets/").Handler(assetHandler)
+
+	fmt.Printf("Starting the server on :%d...\n", cfg.Port)
+	http.ListenAndServe(fmt.Sprintf(":%d", cfg.Port), csrfMw(userMw.Apply(r)))
+
 }
